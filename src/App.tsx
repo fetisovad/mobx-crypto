69fa8aa668311367179309f9e828a759eded9d1e
Alexandr Fetisov <a.d.fetisov@tmn3.etagi.com>
2022-02-05 21:05:52 +0500

    second video 2:24 min

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,18 @@
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
+import React from 'react';
 import {
   Container,
   Grid,
 } from '@mui/material/';
 
 import { CryptoTable, ConverterBlock } from './components';
-import { TCoin } from './types';
 
 function App() {
-  const [allCoins, setAllCoins] = useState<TCoin[]>([]);
-
-  useEffect(() => {
-    axios
-      .get('https://min-api.cryptocompare.com/data/top/totalvolfull?limit=10&tsym=USD')
-      .then(({ data }) => {
-        const coins: TCoin[] = data.Data.map((coin: any) => {
-          const obj: TCoin = {
-            name: coin.CoinInfo.Name,
-            fullName: coin.CoinInfo.FullName,
-            imageUrl: `https://www.cryptocompare.com/${coin.CoinInfo.ImageUrl}`,
-            price: coin.RAW.USD.PRICE.toFixed(3),
-            volume24Hour: parseInt(coin.RAW.USD.VOLUME24HOUR),
-          };
-          return obj;
-        });
-        setAllCoins(coins);
-      });
-  }, []);
-
   return (
     <div className="App">
       <Container maxWidth="lg">
         <Grid container spacing={3} padding={10}>
           <Grid item xs={8} textAlign="center" color="secondary">
-            <CryptoTable items={allCoins} />
+            <CryptoTable/>
           </Grid>
           <Grid item xs={4} textAlign="center" padding={2} color="secondary">
             <ConverterBlock  />
--- src/components/CryptoTable/index.tsx
+++ src/components/CryptoTable/index.tsx
@@ -1,4 +1,7 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { observer, inject } from 'mobx-react';
+import CurrenciesStore from '../../stores/currenciesStore';
+
 import {
   Table,
   TableBody,
@@ -17,43 +20,53 @@ const CoinIconStyle = {
 };
 
 type ICryptoTable = {
-  items: TCoin[];
+  currenciesStore?: CurrenciesStore;
 }
 
-const CryptoTable = ({ items }: ICryptoTable) => {
-  return (
-    <TableContainer component={Paper}>
-      <Table sx={{ minWidth: 650 }} aria-label="simple table">
-        <TableHead>
-          <TableRow>
-            <TableCell />
-            <TableCell align="left">Name</TableCell>
-            <TableCell align="left">FullName</TableCell>
-            <TableCell align="left">Price</TableCell>
-            <TableCell align="left">volume24hour</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {!items.length
-            ? 'Загрузка...'
-            : items.map((coin) => (
-                <TableRow
-                  key={coin.name}
-                  sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-                >
-                  <TableCell align="left">
-                    <img style={CoinIconStyle} src={coin.imageUrl} alt="Coin icon" />
-                  </TableCell>
-                  <TableCell align="left">{coin.name}</TableCell>
-                  <TableCell align="left">{coin.fullName}</TableCell>
-                  <TableCell align="left">$ {coin.price}</TableCell>
-                  <TableCell align="left">$ {coin.volume24Hour}</TableCell>
-                </TableRow>
-              ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
-  );
-};
+const CryptoTable = inject('currenciesStore')(
+  observer(({currenciesStore}: ICryptoTable) => {
+    const items: TCoin[] = currenciesStore!.getItems;
+
+    useEffect(() => {
+      if(currenciesStore) {
+        currenciesStore.fetchCoins();
+      }
+    }, [])
+
+    return (
+      <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 650 }} aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell />
+              <TableCell align="left">Name</TableCell>
+              <TableCell align="left">FullName</TableCell>
+              <TableCell align="left">Price</TableCell>
+              <TableCell align="left">volume24hour</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {!items.length
+              ? 'Загрузка...'
+              : items.map((coin) => (
+                  <TableRow
+                    key={coin.name}
+                    sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                  >
+                    <TableCell align="left">
+                      <img style={CoinIconStyle} src={coin.imageUrl} alt="Coin icon" />
+                    </TableCell>
+                    <TableCell align="left">{coin.name}</TableCell>
+                    <TableCell align="left">{coin.fullName}</TableCell>
+                    <TableCell align="left">$ {coin.price}</TableCell>
+                    <TableCell align="left">$ {coin.volume24Hour}</TableCell>
+                  </TableRow>
+                ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    );
+  })
+);
 
 export default CryptoTable;
